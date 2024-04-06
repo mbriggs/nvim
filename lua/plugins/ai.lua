caa5bddd040e796da24024ed0e065911db368fc9
Matt Briggs <matt@mattbriggs.net>
2024-04-06 09:18:03 -0700

    switch to copilot.lua

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,3 +1,23 @@
+-- Github Copilot
+-- Mappings are in alacritty config
+-- cmd-; - accept
+-- cmd-] - next
+-- cmd-[ - prev
 return {
-	{ "github/copilot.vim" },
+    {
+        "zbirenbaum/copilot.lua",
+        main = "copilot",
+        event = "VeryLazy",
+        config = function()
+            require("copilot").setup({
+                suggestion = {
+                    auto_trigger = true,
+                },
+                file_types = {
+                    ["*"] = true,
+                },
+            })
+            vim.cmd([[Copilot auth]])
+        end,
+    }
 }
