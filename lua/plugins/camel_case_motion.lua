de0b29e75fd45e661ec4d81f1fd74762a674ae11
Matt Briggs <matt@mattbriggs.net>
2024-04-06 16:13:30 -0700

    switch to camel case motion
    does what i want it to

---
+++ lua/plugins/camel_case_motion.lua
@@ -0,0 +1,7 @@
+return {
+	"bkad/CamelCaseMotion",
+	event = "BufRead",
+	init = function()
+		vim.g.camelcasemotion_key = ","
+	end,
+}
--- lua/plugins/wordmotion.lua
+++
@@ -1,5 +0,0 @@
-return {
-    "chaoren/vim-wordmotion",
-    event = "BufRead",
-    setup = [[vim.g.wordmotion_prefix = '<space>']],
-}
