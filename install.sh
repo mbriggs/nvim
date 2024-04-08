fe5c953a3fd24d19124677a9ed0c2545ae1d4445
Matt Briggs <matt@mattbriggs.net>
2024-04-08 08:05:30 -0700

    copilot chat integration

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+ln -sfv "$HOME/.config/nvim" "$(dirname "$0")"
+
+brew install luarocks
+brew install neovim
+brew install python3
+brew install tmux
+brew install ripgrep
+brew install fd
+brew install httpie
+brew install jq
+brew install pgcli
+
+luarocks install --lua-version 5.1 tiktoken_core # for copilotchat
