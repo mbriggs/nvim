be2ddfc920602ac8bd401094ff3c7f97203988e2
Matt Briggs <matt@mattbriggs.net>
2024-04-08 16:25:56 -0700

    switch from fugitive to lazygit

--- lua/plugins/fugitive.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"tpope/vim-fugitive",
-	keys = {
-		{ "<leader>gg", "<cmd>:Git<CR>", noremap = true, silent = true, desc = "Git status" },
-		{ "<leader>gp", "<cmd>:Git push<CR>", noremap = true, silent = true, desc = "Push" },
-		{ "<leader>gf", "<cmd>:Git pull --rebase<CR>", noremap = true, silent = true, desc = "Pull with rebase" },
-		{ "<leader>gt", "<cmd>:Git push -u origin<CR>", noremap = true, silent = true, desc = "Push with tracking" },
-	},
-}
