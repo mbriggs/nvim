b732354c2dd22fea321730e50870357117839cc1
Matt Briggs <matt@mattbriggs.net>
2024-04-19 07:53:56 -0700

    add git blame

---
+++ lua/plugins/blame.lua
@@ -0,0 +1,10 @@
+return {
+    "FabijanZulj/blame.nvim",
+    name = "blame",
+    opts = {
+        date_format = "%Y.%m.%d",
+    },
+    keys = {
+        { "<leader>gb", "<cmd>BlameToggle<cr>", desc = "Blame toggle" },
+    }
+}
