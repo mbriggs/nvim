d53313199114de82caf1c7ecffe8a98e311aa786
Matt Briggs <matt@mattbriggs.net>
2024-05-20 08:38:40 -0400

    neovim 0.10.0 and switch to kitty

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -4,24 +4,29 @@
 -- cmd-] - next
 -- cmd-[ - prev
 return {
-  "zbirenbaum/copilot.lua",
-  main = "copilot",
-  event = "VeryLazy",
-  config = function()
-    require("copilot").setup({
-      filetypes = {
-        org = false,
-      },
-      suggestion = {
-        auto_trigger = true,
-      },
-      panel = {
-        enabled = false,
-      },
-      file_types = {
-        ["*"] = true,
-      },
-    })
-    vim.cmd([[Copilot auth]])
-  end,
+    "zbirenbaum/copilot.lua",
+    main = "copilot",
+    event = "VeryLazy",
+    config = function()
+        require("copilot").setup({
+            filetypes = {
+                org = false,
+            },
+            suggestion = {
+                auto_trigger = true,
+                keymap = {
+                    accept = "<M-;>",
+                    accept_word = false,
+                    accept_line = false,
+                    next = "<M-]>",
+                    prev = "<M-[>",
+                    dismiss = "<C-]>",
+                },
+            },
+            panel = {
+                enabled = false,
+            },
+        })
+        vim.cmd([[Copilot auth]])
+    end,
 }
