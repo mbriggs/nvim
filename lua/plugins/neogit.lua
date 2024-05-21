319ad402c3823c1468ed63042dee7cd5dae380ad
Matt Briggs <matt@mattbriggs.net>
2024-05-21 10:03:04 -0400

    dont show console in neogit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,15 +1,16 @@
 return {
-    "NeogitOrg/neogit",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "sindrets/diffview.nvim",
-        "nvim-telescope/telescope.nvim",
-    },
-    opts = {
-        kind = "auto",
-    },
-    cmd = { "Neogit" },
-    keys = {
-        { "<leader>gg", "<cmd>Neogit<CR>", desc = "Neogit" },
-    },
+	"NeogitOrg/neogit",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"sindrets/diffview.nvim",
+		"nvim-telescope/telescope.nvim",
+	},
+	opts = {
+		kind = "auto",
+		auto_show_console = false,
+	},
+	cmd = { "Neogit" },
+	keys = {
+		{ "<leader>gg", "<cmd>Neogit<CR>", desc = "Neogit" },
+	},
 }
