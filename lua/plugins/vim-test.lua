946572d2ac1f55759d77501fed75ee31c59a27c5
Matt Briggs <matt@mattbriggs.net>
2024-05-23 07:36:26 -0400

    Switch to kitty

--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -1,22 +1,18 @@
 return {
-    'vim-test/vim-test',
-    dependencies = {
-        'jgdavey/tslime.vim'
-
-    },
+    "vim-test/vim-test",
 
     init = function()
         vim.g.tslime_always_current_session = 1
         vim.g.tslime_always_current_window = 1
 
-        vim.g["test#strategy"] = 'tslime'
+        vim.g["test#strategy"] = "kitty"
     end,
 
     keys = {
-        { '<leader>tt', '<cmd>TestNearest<cr>', desc = "Test Nearest" },
-        { '<leader>tf', '<cmd>TestFile<cr>',    desc = "Test Current File" },
-        { '<leader>ta', '<cmd>TestSuite<cr>',   desc = "Test Whole Suite" },
-        { '<leader>tl', '<cmd>TestLast<cr>',    desc = "Rerun last test" },
-        { '<leader>tg', '<cmd>TestVisit<cr>',   desc = "Visit last test" },
-    }
+        { "<leader>tt", "<cmd>TestNearest<cr>", desc = "Test Nearest" },
+        { "<leader>tf", "<cmd>TestFile<cr>",    desc = "Test Current File" },
+        { "<leader>ta", "<cmd>TestSuite<cr>",   desc = "Test Whole Suite" },
+        { "<leader>tl", "<cmd>TestLast<cr>",    desc = "Rerun last test" },
+        { "<leader>tg", "<cmd>TestVisit<cr>",   desc = "Visit last test" },
+    },
 }
