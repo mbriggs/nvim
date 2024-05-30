da40dd4790d48e9864c695b8c9e86f707221e71e
Matt Briggs <matt@mattbriggs.net>
2024-05-30 08:10:42 -0400

    redo blame

--- lua/plugins/blame.lua
+++ lua/plugins/blame.lua
@@ -1,10 +1,14 @@
 return {
-    "FabijanZulj/blame.nvim",
-    name = "blame",
-    opts = {
-        date_format = "%Y.%m.%d",
-    },
-    keys = {
-        { "<leader>gb", "<cmd>BlameToggle<cr>", desc = "Blame toggle" },
-    }
+  "f-person/git-blame.nvim",
+  name = "gitblame",
+  opts = {
+    enabled = false,
+    date_format = "%Y.%m.%d",
+  },
+  keys = {
+    { "<leader>gbb", "<cmd>GitBlameToggle<cr>",        desc = "Blame toggle" },
+    { "<leader>gbs", "<cmd>GitBlameCopySha<cr>",       desc = "Copy SHA" },
+    { "<leader>gbu", "<cmd>GitBlameOpenCommitURL<cr>", desc = "Open commit url" },
+    { "<leader>gby", "<cmd>GitBlameCopyCommitURL<cr>", desc = "Copy commit url" },
+  },
 }
