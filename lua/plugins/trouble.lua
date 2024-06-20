27534f1c4e5ff828b344a632dd589c8b9e88fdc4
Matt Briggs <matt@mattbriggs.net>
2024-06-19 20:23:02 -0400

    use x for trouble

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,9 +1,9 @@
 return {
-	"folke/trouble.nvim",
-	cmd = "Trouble",
-	config = true,
-	keys = {
-		{ "<leader>qq", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
-		{ "<leader>qb", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
-	},
+  "folke/trouble.nvim",
+  cmd = "Trouble",
+  config = true,
+  keys = {
+    { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>",              desc = "Diagnostics (Trouble)" },
+    { "<leader>xb", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+  },
 }
