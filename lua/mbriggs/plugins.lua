0c3a22c8c7554a49b7c794feeae9e43e3e5da5cc
Matt Briggs <matt@mattbriggs.net>
2024-06-28 06:46:44 -0400

    map key for lazy

--- lua/mbriggs/plugins.lua
+++ lua/mbriggs/plugins.lua
@@ -1,15 +1,17 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins", { change_detection = { notify = false } });
-vim.cmd.packadd('cfilter') -- use cfilter for filtering quickfix list
+require("lazy").setup("plugins", { change_detection = { notify = false } })
+vim.cmd.packadd("cfilter") -- use cfilter for filtering quickfix list
+
+vim.keymap.set("n", "<leader>z", "<cmd>Lazy<cr>", { desc = "Lazy Package Manager" })
