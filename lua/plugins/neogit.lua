ba4dd8151fad4960e63a62c15022a899ea18a78c
Matt Briggs <matt@mattbriggs.net>
2024-07-24 08:16:51 -0400

    use lazygit instead of neogit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,21 +1,22 @@
-return {
-	"NeogitOrg/neogit",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"sindrets/diffview.nvim",
-		"nvim-telescope/telescope.nvim",
-	},
-	opts = {
-		kind = "auto",
-		console_timeout = 10000,
-		auto_show_console = false,
-		integrations = {
-			diffview = true,
-			telescope = true,
-		},
-	},
-	cmd = { "Neogit" },
-	keys = {
-		{ "<leader>gg", "<cmd>Neogit<CR>", desc = "Neogit" },
-	},
-}
+return {}
+-- return {
+-- 	"NeogitOrg/neogit",
+-- 	dependencies = {
+-- 		"nvim-lua/plenary.nvim",
+-- 		"sindrets/diffview.nvim",
+-- 		"nvim-telescope/telescope.nvim",
+-- 	},
+-- 	opts = {
+-- 		kind = "auto",
+-- 		console_timeout = 10000,
+-- 		auto_show_console = false,
+-- 		integrations = {
+-- 			diffview = true,
+-- 			telescope = true,
+-- 		},
+-- 	},
+-- 	cmd = { "Neogit" },
+-- 	keys = {
+-- 		{ "<leader>gg", "<cmd>Neogit<CR>", desc = "Neogit" },
+-- 	},
+-- }
