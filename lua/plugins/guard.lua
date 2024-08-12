d80b2c1632f30cf46ad578cc9ebd69119c319322
Matt Briggs <matt@mattbriggs.net>
2024-08-12 09:39:58 -0400

    pull back a bit on guard

--- lua/plugins/guard.lua
+++ lua/plugins/guard.lua
@@ -1,25 +1,23 @@
 return {
-    "nvimdev/guard.nvim",
-    dependencies = {
-        "nvimdev/guard-collection",
-    },
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-        local ft = require("guard.filetype")
+	"nvimdev/guard.nvim",
+	dependencies = {
+		"nvimdev/guard-collection",
+	},
+	event = { "BufReadPost", "BufNewFile" },
+	config = function()
+		local ft = require("guard.filetype")
 
-        ft("lua"):fmt("lsp"):append("stylua")
+		ft("lua"):fmt("lsp"):append("stylua")
+		ft("typescript,javascript,typescriptreact"):fmt("prettier")
+		ft("go"):fmt({ cmd = "goimports", stdin = true })
+		ft("sh"):fmt("shfmt")
+		ft("templ"):fmt({ cmd = "templ", args = { "fmt" }, stdin = true })
+		-- ft("ruby"):fmt("rubocop")
+		ft("sql"):fmt("sql-formatter")
 
-        ft("typescript,javascript,typescriptreact"):fmt("prettier")
-
-        ft("go"):fmt({ cmd = "goimports", stdin = true })
-
-        ft("sh"):fmt("shfmt")
-
-        ft("templ"):fmt({ cmd = "templ", args = { "fmt" }, stdin = true })
-
-        require("guard").setup({
-            fmt_on_save = true,
-            lsp_as_default_formatter = true,
-        })
-    end,
+		require("guard").setup({
+			fmt_on_save = true,
+			lsp_as_default_formatter = true,
+		})
+	end,
 }
