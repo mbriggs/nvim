f5409a03abb80daad8ddae26292e7f53cdf48882
Matt Briggs <matt@mattbriggs.net>
2024-08-12 09:39:02 -0400

    dont use copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,30 +3,31 @@
 -- cmd-; - accept
 -- cmd-] - next
 -- cmd-[ - prev
-return {
-  "zbirenbaum/copilot.lua",
-  main = "copilot",
-  event = "VeryLazy",
-  config = function()
-    require("copilot").setup({
-      filetypes = {
-        org = false,
-      },
-      suggestion = {
-        auto_trigger = true,
-        keymap = {
-          accept = "<D-;>",
-          accept_word = false,
-          accept_line = false,
-          next = "<D-]>",
-          prev = "<D-[>",
-          dismiss = "<C-]>",
-        },
-      },
-      panel = {
-        enabled = false,
-      },
-    })
-    vim.cmd([[Copilot auth]])
-  end,
-}
+return {}
+-- return {
+-- 	"zbirenbaum/copilot.lua",
+-- 	main = "copilot",
+-- 	event = "VeryLazy",
+-- 	config = function()
+-- 		require("copilot").setup({
+-- 			filetypes = {
+-- 				org = false,
+-- 			},
+-- 			suggestion = {
+-- 				auto_trigger = true,
+-- 				keymap = {
+-- 					accept = "<D-;>",
+-- 					accept_word = false,
+-- 					accept_line = false,
+-- 					next = "<D-]>",
+-- 					prev = "<D-[>",
+-- 					dismiss = "<C-]>",
+-- 				},
+-- 			},
+-- 			panel = {
+-- 				enabled = false,
+-- 			},
+-- 		})
+-- 		vim.cmd([[Copilot auth]])
+-- 	end,
+-- }
