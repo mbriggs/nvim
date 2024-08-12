06092c5dac956b6709239e47084fc10c52c8e025
Matt Briggs <matt@mattbriggs.net>
2024-08-12 09:39:20 -0400

    kitty navigate in insert mode

--- lua/plugins/kitty-navigator.lua
+++ lua/plugins/kitty-navigator.lua
@@ -1,16 +1,22 @@
 return {
-  "knubie/vim-kitty-navigator",
-  event = "VeryLazy",
-  build = "cp ./*.py ~/.config/kitty/",
-  init = function()
-    vim.g.kitty_navigator_no_mappings = 1
-  end,
-  config = function()
-    local map = vim.keymap.set
+	"knubie/vim-kitty-navigator",
+	event = "VeryLazy",
+	build = "cp ./*.py ~/.config/kitty/",
+	init = function()
+		vim.g.kitty_navigator_no_mappings = 1
+	end,
+	config = function()
+		local map = vim.keymap.set
+		local modes = { "n", "v" }
 
-    map("", "<D-h>", ":KittyNavigateLeft<cr>", { desc = "kitty navigate left" })
-    map("", "<D-j>", ":KittyNavigateDown<cr>", { desc = "kitty navigate down" })
-    map("", "<D-k>", ":KittyNavigateUp<cr>", { desc = "kitty navigate up" })
-    map("", "<D-l>", ":KittyNavigateRight<cr>", { desc = "kitty navigate right" })
-  end,
+		map(modes, "<D-h>", ":KittyNavigateLeft<cr>", { desc = "kitty navigate left" })
+		map(modes, "<D-j>", ":KittyNavigateDown<cr>", { desc = "kitty navigate down" })
+		map(modes, "<D-k>", ":KittyNavigateUp<cr>", { desc = "kitty navigate up" })
+		map(modes, "<D-l>", ":KittyNavigateRight<cr>", { desc = "kitty navigate right" })
+
+		map("i", "<D-h>", "<c-o>:KittyNavigateLeft<cr>", { desc = "kitty navigate left" })
+		map("i", "<D-j>", "<c-o>:KittyNavigateDown<cr>", { desc = "kitty navigate down" })
+		map("i", "<D-k>", "<c-o>:KittyNavigateUp<cr>", { desc = "kitty navigate up" })
+		map("i", "<D-l>", "<c-o>:KittyNavigateRight<cr>", { desc = "kitty navigate right" })
+	end,
 }
